9cb3e45164426adbb63fc44d720e057e84f13438
waldo wautelet <waldowautelet4@gmail.com>
2025-04-13 16:16:35 +0200

    feat: cleanup + button to go to user location

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -1,28 +1,15 @@
-import MapView, {Marker, PROVIDER_GOOGLE, Region} from "react-native-maps";
+import MapView, {LatLng, Marker, PROVIDER_GOOGLE, Region} from "react-native-maps";
 import styles from "./Map.styles";
-import useUserLocation from "@/app/hooks/useUserLocation";
-import {useEffect, useState} from "react";
+import {forwardRef, useState} from "react";
 import initialMapRegion from "@/app/components/Map/InitialMapRegion";
 
-const Map = () => {
+type Props = {
+    location?: LatLng;
+}
+
+const Map = forwardRef<MapView, Props>(({location}: Props, ref) => {
     const [region, setRegion] = useState<Region>(initialMapRegion);
-    const [mapRef, setMapRef] = useState<MapView>();
-    const [zoomedToUserLocation, setZoomedToUserLocation] = useState<boolean>(false);
-
-    const {location, errorMsg} = useUserLocation();
-
-    useEffect(() => {
-        if (zoomedToUserLocation || !location || !mapRef) return;
-        
-        mapRef.animateToRegion({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.02,
-            longitudeDelta: 0.02,
-        }, 500);
-        setZoomedToUserLocation(true);
-    }, [location, mapRef]);
-
+    
     return (
         <>
             <MapView
@@ -30,13 +17,13 @@ const Map = () => {
                 initialRegion={region}
                 onRegionChangeComplete={setRegion}
                 provider={PROVIDER_GOOGLE}
-                ref={(ref) => ref && setMapRef(ref)}
+                ref={ref}
             >
                 {location &&
-                    <Marker coordinate={{latitude: location.coords.latitude, longitude: location.coords.longitude}}/>}
+                    <Marker coordinate={{latitude: location.latitude, longitude: location.longitude}}/>}
             </MapView>
         </>
     );
-}
+});
 
 export default Map;
--- app/screens/MapJournal/MapJournal.tsx
+++ app/screens/MapJournal/MapJournal.tsx
@@ -4,39 +4,69 @@ import {View, Image} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 
 import styles from "./MapJournal.styles";
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import useUserLocation from "@/app/hooks/useUserLocation";
+import MapView from "react-native-maps";
 
 const MapJournal = () => {
     const [selectedImages, setSelectedImages] = useState<string[]>();
+    const [zoomedToUserLocation, setZoomedToUserLocation] = useState<boolean>(false);
+    
+    const mapRef = useRef<MapView>(null);
+    
+    const { location, errorMsg } = useUserLocation();
+
+    function moveMapToUser(latitude: number, longitude: number) {
+        if (!mapRef.current) return;
+
+        mapRef.current.animateToRegion({
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.02,
+        }, 500);
+    }
+
+    async function handleMoveMapToUserClick() {
+        if (!mapRef.current || !location) return;
+        moveMapToUser(location.coords.latitude, location.coords.longitude);
+    }
+
+    useEffect(() => {
+        if (zoomedToUserLocation || !location || !mapRef.current) return;
+
+        moveMapToUser(location.coords.latitude, location.coords.longitude);
+        setZoomedToUserLocation(true);
+    }, [location, mapRef]);
 
     async function getImages() {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ['images'],
             allowsMultipleSelection: true,
-            aspect: [4, 3]
+            aspect: [4, 3],
+            exif: true
         });
         
         console.log(result);
         
         if (!result.canceled) {
             setSelectedImages(result.assets.map(a => a.uri));
+            console.log(result.assets[0].exif);
         }
     }
 
     return (
         <>
-            <Map/>
+            <Map 
+                location={location?.coords}
+                ref={mapRef}
+            />
             <View
                 style={styles.container}
             >
-                {
-                    selectedImages 
-                    && selectedImages.length > 0 
-                        ? selectedImages.map((image, i) => <Image key={i} style={{ height: 200, width: 200 }} source={{ uri: image}}/>) 
-                        : null
-                }
                 <View style={styles.buttonContainer}>
                     <GenericButton label="+" onPressAsync={getImages} />
+                    <GenericButton label="." onPressAsync={handleMoveMapToUserClick} />
                 </View>
             </View>
         </>
